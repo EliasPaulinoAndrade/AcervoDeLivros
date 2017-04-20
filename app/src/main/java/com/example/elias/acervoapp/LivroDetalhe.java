fbe2140d5a733b07b469c73939f08e28e0215853
Elias <eliaspaulinoandrade@gmail.com>
2017-04-20 11:09:34 -0300

    testando animations

--- app/src/main/java/com/example/elias/acervoapp/LivroDetalhe.java
+++ app/src/main/java/com/example/elias/acervoapp/LivroDetalhe.java
@@ -4,12 +4,12 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -17,8 +17,8 @@ import android.widget.TextView;
 
 import com.example.elias.acervoapp.fragmentos.DefaultLivroFragment;
 import com.example.elias.acervoapp.fragmentos.EditLivroFragment;
-
-import org.w3c.dom.Text;
+import com.example.elias.acervoapp.fragmentos.RemoveLivroFragment;
+import com.example.elias.acervoapp.fragmentos.StatusLivroFragment;
 
 public class LivroDetalhe extends AppCompatActivity {
     @Override
@@ -29,7 +29,7 @@ public class LivroDetalhe extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    private void setClickedButton(View v, int colorBack, int imageIcon, int textColor){
+    private void setClickedButton(View v, int colorBack, int imageIcon, int textColor, Fragment novoFragment){
         v.setBackgroundColor(getResources().getColor(colorBack));
         ViewGroup it = (ViewGroup)v;
         for(int i=0; i<it.getChildCount(); i++){
@@ -42,25 +42,20 @@ public class LivroDetalhe extends AppCompatActivity {
         }
         RelativeLayout detail = (RelativeLayout) findViewById(R.id.livroDetail);
         RelativeLayout.LayoutParams params =(RelativeLayout.LayoutParams) detail.getLayoutParams();
-        TextView titleDetail = (TextView) detail.findViewById(R.id.titleLivroDetail);
-        getSupportActionBar().setTitle(titleDetail.getText());
-        getSupportActionBar().setDisplayShowTitleEnabled(true);
         params.height = 0;
         detail.setLayoutParams(params);
 
-        Fragment editFrag = new EditLivroFragment();
+
+        TextView titleDetail = (TextView) detail.findViewById(R.id.titleLivroDetail);
+        getSupportActionBar().setTitle(titleDetail.getText());
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
         FragmentManager fragMan = getSupportFragmentManager();
         FragmentTransaction fragTran = fragMan.beginTransaction();
-        fragTran.replace(R.id.fragmentoDetail, editFrag);
+        fragTran.replace(R.id.fragmentoDetail, novoFragment);
         fragTran.commit();
     }
-    public void editLivroClick(View v){
-        setClickedButton(v, R.color.firstYellow, R.drawable.grayedit, R.color.secGray);
-    }
-    public void removeLivroClick(View v){
-        setClickedButton(v, R.color.firstRed, R.drawable.graylixo, R.color.secGray);
-    }
-    public void imageClick(View v){
+    private void unsetClickedButton(FrameLayout button, int imageIcon){
         RelativeLayout detail = (RelativeLayout) findViewById(R.id.livroDetail);
         detail.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -71,15 +66,43 @@ public class LivroDetalhe extends AppCompatActivity {
         fragTran.replace(R.id.fragmentoDetail, defaultFrag);
         fragTran.commit();
 
-        FrameLayout editFrame = (FrameLayout) findViewById(R.id.editButton);
-        editFrame.setBackgroundColor(getResources().getColor(R.color.firstGray));
-        for(int i=0; i<editFrame.getChildCount(); i++){
-            if(editFrame.getChildAt(i) instanceof ImageView){
-                ((ImageView) editFrame.getChildAt(i)).setImageResource(R.drawable.lapis);
+        button.setBackgroundColor(getResources().getColor(R.color.firstGray));
+        for(int i=0; i<button.getChildCount(); i++){
+            if(button.getChildAt(i) instanceof ImageView){
+                ((ImageView) button.getChildAt(i)).setImageResource(imageIcon);
             }
-            if(editFrame.getChildAt(i) instanceof TextView){
-                ((TextView) editFrame.getChildAt(i)).setTextColor(getResources().getColor(R.color.white));
+            if(button.getChildAt(i) instanceof TextView){
+                ((TextView) button.getChildAt(i)).setTextColor(getResources().getColor(R.color.white));
             }
         }
+    }
+    public void editLivroClick(View v){
+        FrameLayout statusFrame = (FrameLayout) findViewById(R.id.statusButton);
+        FrameLayout removeFrame = (FrameLayout) findViewById(R.id.removeButton);
+        unsetClickedButton(removeFrame, R.drawable.lixo);
+        unsetClickedButton(statusFrame, R.drawable.status);
+        setClickedButton(v, R.color.firstYellow, R.drawable.grayedit, R.color.secGray, new EditLivroFragment());
+    }
+    public void removeLivroClick(View v){
+        FrameLayout statusFrame = (FrameLayout) findViewById(R.id.statusButton);
+        FrameLayout editFrame = (FrameLayout) findViewById(R.id.editButton);
+        unsetClickedButton(editFrame, R.drawable.lapis);
+        unsetClickedButton(statusFrame, R.drawable.status);
+        setClickedButton(v, R.color.firstGreen, R.drawable.graylixo, R.color.secGray, new RemoveLivroFragment());
+    }
+    public void statusLivroClick(View v){
+        FrameLayout removeFrame = (FrameLayout) findViewById(R.id.removeButton);
+        FrameLayout editFrame = (FrameLayout) findViewById(R.id.editButton);
+        unsetClickedButton(removeFrame, R.drawable.lixo);
+        unsetClickedButton(editFrame, R.drawable.lapis);
+        setClickedButton(v, R.color.firstRed, R.drawable.statusgray, R.color.secGray, new StatusLivroFragment());
+    }
+    public void imageClick(View v){
+        FrameLayout editFrame = (FrameLayout) findViewById(R.id.editButton);
+        FrameLayout removeFrame = (FrameLayout) findViewById(R.id.removeButton);
+        FrameLayout statusFrame = (FrameLayout) findViewById(R.id.statusButton);
+        unsetClickedButton(editFrame, R.drawable.lapis);
+        unsetClickedButton(removeFrame, R.drawable.lixo);
+        unsetClickedButton(statusFrame, R.drawable.status);
     }
 }
